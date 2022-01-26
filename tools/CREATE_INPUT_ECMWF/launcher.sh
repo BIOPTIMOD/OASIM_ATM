c3e2ebf7f1ef3b7030d0df38ebcc7f38e016070d
Giorgio Bolzon <gbolzon@inogs.it>
2022-01-26 15:03:15 +0100

    restoring previous commit 521b944cb41b5

--- tools/CREATE_INPUT_ECMWF/launcher.sh
+++ tools/CREATE_INPUT_ECMWF/launcher.sh
@@ -9,13 +9,7 @@ do
 
 echo "$yyyymmdd"
 
-INPUTFILE=/g100_scratch/userexternal/plazzari/OASIM/ECMWF/ERAINTERIM_2017.nc
-OUTFILE=/g100_scratch/userexternal/OASIM/output_${yyyymmdd}.nc
-
-
-
-
-python3 create_opt_nc.py -i $INPUTFILE -o $OUTFILE -d ${yyyymmdd} -m mask.nc
+python3  create_opt_nc.py    ${yyyymmdd}
 python3  create_cloud_nc.py  ${yyyymmdd}
 
 done < lista_date.txt
