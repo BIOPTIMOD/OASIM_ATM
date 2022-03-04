200ab8d57dd68948cab7611e4504a28621e3c3d0
Giorgio Bolzon <gbolzon@inogs.it>
2022-03-04 10:45:16 +0100

    Establishing operational names and units of OASIM preproc, for daily files provided by CMCC Preproc phase performs only cut and interpolation, no manipulation of variables. The only manipulated variable is the module of U10M, V10M.
    We provide an output file for every time.

--- tools/CREATE_INPUT_ECMWF/create_opt_nc.py
+++ tools/CREATE_INPUT_ECMWF/create_opt_nc.py
@@ -28,25 +28,14 @@ def argument():
 args = argument()
 
 
-import sys
-from datetime import datetime
+import os
+from datetime import datetime, timedelta
 from commons import netcdf4
 import numpy as np
 from scipy import interpolate
 from commons.mask import Mask
 import netCDF4
 from commons.utils import addsep
-    
-def set_nan_values(M, fill_val):
-    bad=np.isnan(M) | M < 0
-    M[bad] = fill_val
-
-
-nframes_in_day=4
-
-#d=datetime.strptime(args.date,"%Y%m%d")
-#frame_start = ( int(d.strftime("%j")) - 1 ) * nframes_in_day
-#frame_end   = frame_start + nframes_in_day
 
 
 
@@ -54,6 +43,15 @@ TheMask=Mask(args.maskfile)
 jpk,jpj,jpi = TheMask.shape
 filename=args.inputfile
 OUTDIR = addsep(args.outdir)
+
+
+ncIN=netCDF4.Dataset(filename)
+nframes_in_day = ncIN.dimensions['time'].size
+ncIN.close()
+deltaH = int(24/nframes_in_day)
+yyyymmdd=os.path.basename(filename)[:8]
+
+
 
 lon= netcdf4.readfile(filename, 'lon')
 lat= netcdf4.readfile(filename, 'lat')
@@ -89,30 +87,57 @@ def getframe(filename,var, timeframe):
     M2d_orig = readframe(filename, var, timeframe)
     return  interp(M2d_orig)
 
-def dumpfile(filename, sp,si10,t2m,d2m,msl):
+def dumpfile(filename, maskObj, sp,msl, t2m,d2m, tcc,w10):
     ncOUT   = netCDF4.Dataset(filename,"w");
 
     ncOUT.createDimension('lon',jpi);
     ncOUT.createDimension('lat',jpj);
 
+    ncvar=ncOUT.createVariable('lon','f',('lon',))
+    setattr(ncvar,'units','degrees')
+    setattr(ncvar,'long_name'    ,'longitude')
+    ncvar[:]=maskObj.xlevels[0,:]
+    ncvar=ncOUT.createVariable('lat','f',('lat',))
+    setattr(ncvar,'units','degrees')
+    setattr(ncvar,'long_name'    ,'latitude')
+    ncvar[:]=maskObj.ylevels[:,0]
+
 
     ncvar = ncOUT.createVariable('sp','f',('lat','lon'))
     ncvar[:]=sp
-    setattr(ncvar, 'long_name',  'surface pressure' )
-    setattr(ncvar, 'units',  '[mb]' )
+    setattr(ncvar, 'long_name',  'Surface pressure' )
+    setattr(ncvar, 'units','Pa' )
+    setattr(ncvar, 'code', 34 )
     
-    ncvar = ncOUT.createVariable('wsmorg','f',('lat','lon'))
-    ncvar[:] = si10
-    setattr(ncvar, 'long_name',  '10 m wind speed' )
-    setattr(ncvar, 'units',  '[m.s-1]' )
-    
+
     ncvar = ncOUT.createVariable('msl','f',('lat','lon'))
+    setattr(ncvar, 'units','Pa' )
+    setattr(ncvar,'long_name','Mean sea-level pressure')
+    setattr(ncvar,'code',151)
     ncvar[:] = msl
-    
+
     ncvar = ncOUT.createVariable('d2m','f',('lat','lon'))
+    setattr(ncvar,'units','K')
+    setattr(ncvar,'long_name','2 metre dewpoint temperature')
+    setattr(ncvar, 'code', 168)
     ncvar[:] = d2m
 
     ncvar = ncOUT.createVariable('t2m','f',('lat','lon'))
+    setattr(ncvar,'units','K')
+    setattr(ncvar,'long_name','2 metre temperature')
+    setattr(ncvar, 'code', 167)    
+    ncvar[:] = t2m
+
+    ncvar = ncOUT.createVariable('tcc','f',('lat','lon'))
+    setattr(ncvar,'units','%')
+    setattr(ncvar,'long_name','Total cloud cover')
+    setattr(ncvar, 'code', 164)
+    ncvar[:] = tcc
+
+    ncvar = ncOUT.createVariable('w10','f',('lat','lon'))
+    setattr(ncvar,'units','m/s')
+    setattr(ncvar,'long_name','10 metre wind speed module')
+    setattr(ncvar,'code', '165 and 166')
     ncvar[:] = t2m
 
     setattr(ncOUT, 'input_file', args.inputfile)
@@ -120,75 +145,23 @@ def dumpfile(filename, sp,si10,t2m,d2m,msl):
 
 
 for iframe in range(nframes_in_day):
-    outfile = OUTDIR + "ave.20190101-00:00:00.nc"
+    d=datetime.strptime(yyyymmdd,'%Y%m%d') + timedelta(hours = deltaH*iframe)
+    outfile = OUTDIR + d.strftime("atm.%Y%m%d-%H:%M:%S.nc")
+    print(outfile)
     
     
-    msl = getframe(filename,'MSL', iframe)
-    sp =  getframe(filename,'SP', iframe)
+    msl = getframe(filename,'MSL' , iframe)
+    sp =  getframe(filename,'SP'  , iframe)
     u10 = getframe(filename,'U10M', iframe)
     v10 = getframe(filename,'V10M', iframe)
-    t2m = getframe(filename,'T2M', iframe)
-    d2m = getframe(filename,'D2M', iframe)
+    t2m = getframe(filename,'T2M' , iframe)
+    d2m = getframe(filename,'D2M' , iframe)
+    tcc = getframe(filename,'TCC' , iframe)
 
-    si10 = np.sqrt(u10**2 + v10**2)
-    dumpfile(outfile, sp, si10, t2m, d2m,msl)
+    w10 = np.sqrt(u10**2 + v10**2)
+    dumpfile(outfile, TheMask, sp,msl, t2m,d2m, tcc,w10)
     
-    import sys
-    sys.exit()
 
-    
-    
-    
-    
-    
-# ncin=NC4.Dataset(args.inputfile,"r")
-# 
-# 
-# 
-# 
-# msl =  ncin.variables['msl'][frame_start:frame_end,1:181,:] # Mean sea level pressurea Pascal
-# sp  =  ncin.variables['sp' ][frame_start:frame_end,1:181,:]  # surface pressure Pascal
-# u10 =  ncin.variables['u10'][frame_start:frame_end,1:181,:] # 10 metres u wind speed
-# v10 =  ncin.variables['v10'][frame_start:frame_end,1:181,:] # 10 metres v wind speed
-# # relative humidity and
-# # precipitable water (absorption by water vapour)
-# t2m  = ncin.variables['t2m'][frame_start:frame_end,1:181,:] # 2m temperature K
-# d2m =  ncin.variables['d2m'][frame_start:frame_end,1:181,:] # dew temperature K
-# ozo =  ncin.variables['tco3'][frame_start:frame_end,1:181,:] # total column ozone
-# 
-# 
-# slporg=np.zeros(msl.shape)
-# sporg =np.zeros(sp.shape)
-# 
-# nTimes, nLat, nLon  = slporg.shape
-# 
-# for t in range(nTimes):
-#     for j in range(nLat):
-#         for i  in range(nLon):
-#              slporg[t,j,i] = msl[t,nLat-1-j,i] / 100. # convert to mb
-#              sporg[t,j,i]  = sp[t,nLat-1-j,i]  / 100. # convert to mb
-# 
-# 
-# set_nan_values(slporg, -1.0)
-# set_nan_values(sporg, -1.0)
-# 
-# 
-# 
-# 
-# si10 = np.sqrt(u10*u10 + v10*v10)
-# wsmorg=np.zeros(si10.shape)
-# 
-# nTimes, nLat, nLon  = wsmorg.shape
-# 
-# for t in range(nTimes):
-#     for j in range(nLat):
-#         for i  in range(nLon):
-#              wsmorg[t,j,i] = si10[t,nLat-1-j,i]
-# 
-# set_nan_values(wsmorg, -1.0)
-# 
-# 
-# 
 # a1 = 611.21 # Pascal
 # a3 = 17.502 # dimensionless
 # a4 = 32.19  # Kelvin
@@ -196,10 +169,6 @@ for iframe in range(nframes_in_day):
 # b1 = 0.14 * 0.01  # cm/Pascal
 # b2 = 0.21   # cm
 # 
-# rhorg=np.zeros(si10.shape)
-# wvorg=np.zeros(si10.shape)
-# 
-# nTimes, nLat, nLon  = rhorg.shape
 # 
 # for t in range(nTimes):
 #     for j in range(nLat):
@@ -211,85 +180,26 @@ for iframe in range(nframes_in_day):
 #              rhorg[t,j,i] = 100. * es_Td /es_T
 #              wvorg[t,j,i] = b1 * es_Td * sporg[t,j,i]/slporg[t,j,i] + b2
 # 
-# set_nan_values(rhorg, -1.0)
-# set_nan_values(wvorg, -1.0)
-# #
-# # computation of precipitable water [absorption by water vapour] from saturation water vapour pressure es_T as in Garrison and Adler (1990) and then used in Gregg and Casey (1990)
-# #
-# # note that saturation water pressure is computed in 3 ways:
-# #
-# # 1) in Garrison and Adler (1990) by the Tabata relation (Tabata, 1973)
-# #    es_T = 10^(8.42926609 - 1827.17843/T - 71208.271/T^2) in mb
-# #
-# # 2) in Gregg and Carder (1990) they use the method of Lowe (Lowe, 1977)
-# #    es_T = 1013.25*exp(13.3185*t - 1.9760 *t^2 - 0.6445*t^3 - 0.1299*t^4) in mb, where t=1-373.16/T
-# #
-# # 3) in ECMWF model (https://www.ecmwf.int/sites/default/files/elibrary/2015/9211-part-iv-physical-processes.pdf) by the Teten's formula (in Pa, 1 Pa = 0.01 mb) which is the one then used to compute rhorg above
-# #
-# # however, the three relations give the same results, as shown in saturation-water-vapour-pressure.png
-# #
-# # !!!! OCCORRE IMPORTARE ANCHE LA SURFACE PRESSURE sfcpr da ECMWF
-# # !!!! CONTROLLARE SE SIA NECESSARIO DIVIDERE PER 100 IN QUANTO in GarAdl90 si definisce
-# # e = h * es dove e=vapour pressure (ovvero es_Td) e h=relative humidity
-# #
-# 
-# # ozone
-# 
-# 
-# 
-# ozorg=np.zeros(si10.shape)
-# nTimes, nLat, nLon  = ozorg.shape
-# 
-# for t in range(nTimes):
-#     for j in range(nLat):
-#         for i  in range(nLon):
-#              ozorg[t,j,i] = ozo[t,nLat-1-j,i] / 2.1414 / 0.00001
-# 
-# set_nan_values(ozorg, -1.0)
-# 
-# 
-# 
-# ncOUT   = NC4.Dataset(args.outfile,"w");
-# 
-# ncOUT.createDimension('lon',nLon);
-# ncOUT.createDimension('lat',nLat);
-# ncOUT.createDimension('time',nTimes);
-# 
-# #   ncvar = ncOUT.createVariable('slporg','f',('time','lat','lon')); ncvar[:] = sporg; 
-# 
-# ncvar = ncOUT.createVariable('slporg','f',('lat','lon'))
-# ncvar[:] = sporg.mean(axis=0)
-# setattr(ncvar, 'missing_value',-1.0 )
-# setattr(ncvar, 'long_name',  'surface pressure' )
-# setattr(ncvar, 'units',  '[mb]' )
-# 
-# ncvar = ncOUT.createVariable('wsmorg','f',('lat','lon'))
-# ncvar[:] = wsmorg.mean(axis=0)
-# setattr(ncvar, 'missing_value',-1.0 )
-# setattr(ncvar, 'long_name',  '10 m wind speed' )
-# setattr(ncvar, 'units',  '[m.s-1]' )
-# 
-# ncvar = ncOUT.createVariable('rhorg','f',('lat','lon'))
-# ncvar[:] =  rhorg.mean(axis=0)
-# setattr(ncvar, 'missing_value',-1.0 )
-# setattr(ncvar, 'long_name',  'relative humidity');
-# setattr(ncvar, 'units',  '[%]' );
-# 
-# ncvar = ncOUT.createVariable('ozorg' ,'f',('lat','lon'))
-# ncvar[:] =  ozorg.mean(axis=0)
-# setattr(ncvar, 'missing_value',-1.0 )
-# setattr(ncvar, 'long_name',  'ozone');
-# setattr(ncvar, 'unit',  '[DU]' );
-# 
-# 
-# ncvar = ncOUT.createVariable('wvorg','f',('lat','lon'))
-# ncvar[:] = wvorg.mean(axis=0)
-# setattr(ncvar, 'missing_value',-1.0 )
-# setattr(ncvar, 'long_name',  'precipitable water')
-# setattr(ncvar, 'unit',  '[cm]' )
-# 
-# 
-# setattr(ncOUT, 'input_file', args.inputfile)
-# ncOUT.close()
+
+#
+# computation of precipitable water [absorption by water vapour] from saturation water vapour pressure es_T as in Garrison and Adler (1990) and then used in Gregg and Casey (1990)
+#
+# note that saturation water pressure is computed in 3 ways:
+#
+# 1) in Garrison and Adler (1990) by the Tabata relation (Tabata, 1973)
+#    es_T = 10^(8.42926609 - 1827.17843/T - 71208.271/T^2) in mb
+#
+# 2) in Gregg and Carder (1990) they use the method of Lowe (Lowe, 1977)
+#    es_T = 1013.25*exp(13.3185*t - 1.9760 *t^2 - 0.6445*t^3 - 0.1299*t^4) in mb, where t=1-373.16/T
+#
+# 3) in ECMWF model (https://www.ecmwf.int/sites/default/files/elibrary/2015/9211-part-iv-physical-processes.pdf) by the Teten's formula (in Pa, 1 Pa = 0.01 mb) which is the one then used to compute rhorg above
+#
+# however, the three relations give the same results, as shown in saturation-water-vapour-pressure.png
+#
+# !!!! OCCORRE IMPORTARE ANCHE LA SURFACE PRESSURE sfcpr da ECMWF
+# !!!! CONTROLLARE SE SIA NECESSARIO DIVIDERE PER 100 IN QUANTO in GarAdl90 si definisce
+# e = h * es dove e=vapour pressure (ovvero es_Td) e h=relative humidity
+ 
+
 
 
