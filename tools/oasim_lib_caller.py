ff85d2139cf65144c96194ed1ed67dbcf07934ca
Giorgio Bolzon <gbolzon@inogs.it>
2022-03-08 10:57:03 +0100

    Adding oasim_lib_caller.py, template for call in python the oasim lib compiled in fortran

---
+++ tools/oasim_lib_caller.py
@@ -0,0 +1,33 @@
+import netCDF4 as NC
+import numpy as np
+
+def readfile(filename, var):
+    '''
+    Generic file reader
+    '''
+    D = NC.Dataset(filename, "r")
+    VAR = np.array(D.variables[var])
+    D.close()
+    return VAR
+
+    
+hourly_file="/g100_scratch/userexternal/gbolzon0/OASIM_ATM/out/atm.20190101-18:00:00.nc"
+clim_file  ="/g100_scratch/userexternal/gbolzon0/OASIM_ATM/out/atm.yyyy0101-00:00:00.nc"
+
+
+sp     = readfile(hourly_file,  'sp')
+msl    = readfile(hourly_file, 'mls')
+d2m    = readfile(hourly_file, 'd2m')
+t2m    = readfile(hourly_file, 't2m')
+tcc    = readfile(hourly_file, 'tcc')
+ws10   = readfile(hourly_file, 'w10')
+tclw   = readfile(clim_file,  'tclw')
+tco3   = readfile(clim_file,  'tco3')
+cdrem  = readfile(clim_file, 'cdrem')
+cldtcm = readfile(clim_file,'cldtcm')
+
+
+
+j=100
+result = oasim_lib(sp[j,:],msl[j,:],d2m[j,:],t2m[j,:],tcc[j,:],ws10[j,:],tclw[j,:],tco3[j,:],cdrem[j,:],cldtcm[j,:])
+#result = oasim_lib(sp,msl,d2m,t2m,tcc,ws10,tclw,tco3,cdrem,cldtcm)
