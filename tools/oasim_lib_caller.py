2b50fa47c2848623f97c84931db14bf0ad61550e
Giorgio Bolzon <gbolzon@inogs.it>
2022-03-11 14:55:18 +0100

    Adding 4 arguments to oasim_lib: time, lat,lon,aerosol

--- tools/oasim_lib_caller.py
+++ tools/oasim_lib_caller.py
@@ -1,5 +1,6 @@
 import netCDF4 as NC
 import numpy as np
+from commons.mask import Mask
 
 def readfile(filename, var):
     '''
@@ -13,6 +14,7 @@ def readfile(filename, var):
     
 hourly_file="/g100_scratch/userexternal/gbolzon0/OASIM_ATM/out/atm.20190101-18:00:00.nc"
 clim_file  ="/g100_scratch/userexternal/gbolzon0/OASIM_ATM/out/atm.yyyy0101-00:00:00.nc"
+TheMask    ="/g100_scratch/userexternal/gbolzon0/DEBUG/OGSTM/wrkdir/MODEL/meshmask.nc"
 
 
 sp     = readfile(hourly_file,  'sp')
@@ -25,9 +27,10 @@ tclw   = readfile(clim_file,  'tclw')
 tco3   = readfile(clim_file,  'tco3')
 cdrem  = readfile(clim_file, 'cdrem')
 cldtcm = readfile(clim_file,'cldtcm')
-
+lon = TheMask.xlevels()
+lat = TheMask.ylevels()
 
 
 j=100
-result = oasim_lib(sp[j,:],msl[j,:],d2m[j,:],t2m[j,:],tcc[j,:],ws10[j,:],tclw[j,:],tco3[j,:],cdrem[j,:],cldtcm[j,:])
-#result = oasim_lib(sp,msl,d2m,t2m,tcc,ws10,tclw,tco3,cdrem,cldtcm)
+result = oasim_lib(time, lon[j,:], lat[j,:], sp[j,:],msl[j,:],d2m[j,:],t2m[j,:],tcc[j,:],ws10[j,:],tclw[j,:],tco3[j,:],cdrem[j,:],cldtcm[j,:], aerosol)
+#result = oasim_lib(time, lon, lat, sp,msl,d2m,t2m,tcc,ws10,tclw,tco3,cdrem,cldtcm,aerosol)
