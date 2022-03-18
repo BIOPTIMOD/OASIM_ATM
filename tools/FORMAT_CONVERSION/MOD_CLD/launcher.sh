fed0016af1ea0b4070ccbf3bb179acab87722a7a
Paolo Lazzari <plazzari@ogs.trieste.it>
2022-03-18 17:23:32 +0100

    Adding format conversion scripts

---
+++ tools/FORMAT_CONVERSION/MOD_CLD/launcher.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+./dat2NetCDF.xx modcld0000.dat modcld0000.nc  19000101-00:00:00 19001231-23:59:59
+./dat2NetCDF.xx modcld2000.dat modcld2000.nc  20000101-00:00:00 20001231-23:59:59
+./dat2NetCDF.xx modcld2001.dat modcld2001.nc  20010101-00:00:00 20011231-23:59:59
+./dat2NetCDF.xx modcld2002.dat modcld2002.nc  20020101-00:00:00 20021231-23:59:59
+./dat2NetCDF.xx modcld2003.dat modcld2003.nc  20030101-00:00:00 20031231-23:59:59
+./dat2NetCDF.xx modcld2004.dat modcld2004.nc  20040101-00:00:00 20041231-23:59:59
+./dat2NetCDF.xx modcld2005.dat modcld2005.nc  20050101-00:00:00 20051231-23:59:59
+./dat2NetCDF.xx modcld2006.dat modcld2006.nc  20060101-00:00:00 20061231-23:59:59
